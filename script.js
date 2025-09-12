e764c71544dfef1aac1d0a5c2ecbb3d0a984de3b
nishanishanthi065nisha-beep <nishanthi065nisha@gmail.com>
2025-09-12 11:14:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// Highlight active menu item on scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 60;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+// Add "active" style
+document.querySelectorAll("nav a").forEach((a) => {
+  a.addEventListener("click", () => {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    a.classList.add("active");
+  });
+});
